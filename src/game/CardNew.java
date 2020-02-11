5027935226237c84138a670eaac6fccc2b70a0e8
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-10 19:07:38 -0500

    Abstract class Card is implemented in CardNew class just for demonstration of abstract class implementation

---
+++ src/game/CardNew.java
@@ -0,0 +1,21 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package game;
+
+import project.Card;
+
+/**
+ *
+ * @author Dell
+ */
+public class CardNew extends Card{
+
+    @Override
+    public String toString() {
+        return "Testing...";
+    }
+    
+}
---
+++ src/game/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package game;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+    public static void main(String[]args){
+        CardNew card =new  CardNew();
+        System.out.println(card.toString());
+            
+    }
+}
